3fc162587b8da5b3ee07db6c538b56ed1789732f
ingointo <vishnuvishnumohanms@gmail.com>
2023-09-05 12:44:28 +0530

    changes

--- src/components/screens/Splash.js
+++ src/components/screens/Splash.js
@@ -1,34 +1,35 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import Entypo from '@expo/vector-icons/Entypo';
+import { View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import { useNavigation } from '@react-navigation/native';
-// import RegistrationScreen from './RegistrationScreen'; // Assuming RegistrationScreen uses navigation
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
+
 import LoginScreen from './LoginScreen';
-import OptionScreen from './OptionScreen';
 import BottomDrawer from '../../routes/BottomDrawer';
-import NewCollection from './NewCollection';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 const Splash = () => {
-
-
   const navigation = useNavigation();
   const [appIsReady, setAppIsReady] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false); // Track user login state
 
   useEffect(() => {
     async function prepare() {
       try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(Entypo.font);
+        await Font.loadAsync({}); // Load fonts (add more if needed)
+        // Check if the user is logged in by retrieving a stored token or data
+        const userToken = await AsyncStorage.getItem('userToken');
+        if (userToken) {
+          setUserLoggedIn(true); // User is logged in
+        }
         // Artificially delay for two seconds to simulate a slow loading
         // experience. Please remove this if you copy and paste the code!
-        await new Promise(resolve => setTimeout(resolve, 20));
-      } catch (e) {
-        console.warn(e);
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.error(error);
       } finally {
         // Tell the application to render
         setAppIsReady(true);
@@ -50,10 +51,13 @@ const Splash = () => {
 
   return (
     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      {/* <LoginScreen navigation={navigation} /> */}
-      <BottomDrawer/>
+      {userLoggedIn ? (
+        <BottomDrawer /> // User is logged in, navigate to the main app screen
+      ) : (
+        <LoginScreen navigation={navigation} /> // User is not logged in, show the login screen
+      )}
     </View>
-  )
-}
+  );
+};
 
 export default Splash;
